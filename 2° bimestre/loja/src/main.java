f427b275d99a1fb89766779ba7eb83fa04118839
lelezinha <123339064+lelerudeli@users.noreply.github.com>
2023-05-17 21:06:45 -0300

    atualizei main

--- 2° bimestre/loja/src/main.java
+++ 2° bimestre/loja/src/main.java
@@ -2,9 +2,25 @@ public class main {
 
     public static void main (String args[]) {
 
-        clientes cliente = new clientes ("lele", "39973705831", 989999518);
-        pedidos pedido = new pedidos (cliente, "roupa", 59.90, 9999);
+        clientes cliente = new clientes ("lele", "399.737.058-31", 98999-9518);
+        clientes cliente1 = new clientes ("totolfo", "375.849.274-63", 92777-3846);
+        clientes cliente2 = new clientes ("luquinhas", "384.927.546-37", 93654-7354);
+        
+        pedidos pedido = new pedidos (cliente, "cropped", 59.90, 9999);
+        pedidos pedido2 = new pedidos (cliente2, "calça cargo", 85.90, 1010);
+        pedidos pedido3= new pedidos (cliente3, "moletom", 129.90, 0101);
 
+        
+        loja.addClientes(clientes);
+        loja.addClientes(clientes2);
+        Loja.addClientes(clientes3);
+        
+        loja.addPedidos(pedidos);
+        loja.addPedidos(pedidos2);
+        loja.addPedidos(pedidos3);
+        
+        loja.listarcliente();
+        loja.listarpedidos();
 
 
        // mostrarClientes();
